252615b4252ba9580ba20c63bd9c4af8328efa83
Osirisz <earthstroming@hotmail.com>
2017-04-29 18:52:05 +0700

    add fetch

---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <button v-on:click="getData">Click</button>
+    <p>{{info.stargazers_count}}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+/* eslint-disable */
+export default {
+  name: 'hello',
+  data() {
+    return {
+      info: {
+        getData: {}
+      }
+    };
+  },
+  methods: {
+    getData() {
+      axios.get("https://api.github.com/repos/vuejs/vue")
+      .then(function (response) {
+        console.log(response);
+      })
+     .catch(function (error) {
+        console.log(error);
+    });
+    },
+  }  
+};
+</script>
